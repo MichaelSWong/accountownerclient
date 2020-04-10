029909d8aede622da58606b44330636f39994215
Michael Wong <WongM@fnb-corp.com>
2020-04-10 13:49:50 -0400

    Added 404 not found page

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Layout from '../components/Layout/Layout';
 import Home from '../components/Home/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NotFound from '../components/ErrorPages/NotFound';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path='/' exact component={Home} />
+            <Route path='*' component={NotFound} />
           </Switch>
         </Layout>
       </BrowserRouter>
