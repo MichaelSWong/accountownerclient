d327cf889695517699504117365d3f6b471ee014
Michael Wong <WongM@fnb-corp.com>
2020-04-10 14:41:42 -0400

    added repository actions and reducers

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import repositoryReducer from './store/reducers/repositoryReducer';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(repositoryReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
